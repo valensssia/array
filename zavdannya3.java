f1872a1dbdf72cffc58b971615f37cc02641597f
valensssia <99199656+valensssia@users.noreply.github.com>
2022-05-18 15:14:32 +0300

    Add files via upload

---
+++ zavdannya3.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.Scanner;
+
+public class zavdannya3 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int [] a = new int[n];
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+			if (a[i] > 0) {
+				sum += 1;
+			}
+		}
+		System.out.println(sum);
+			
+		}
+	}
